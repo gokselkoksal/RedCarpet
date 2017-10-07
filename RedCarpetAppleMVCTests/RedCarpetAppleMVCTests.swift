92598e3a6e295d1497f885e0fdc37db48cf7dce6
Goksel Koksal <gokselkoksal@gmail.com>
2017-10-07 22:12:42 +0100

    Add target for Apple-MVC

---
+++ RedCarpetAppleMVCTests/RedCarpetAppleMVCTests.swift
@@ -0,0 +1,36 @@
+//
+//  RedCarpetAppleMVCTests.swift
+//  RedCarpetAppleMVCTests
+//
+//  Created by Göksel Köksal on 7.10.2017.
+//  Copyright © 2017 Packt. All rights reserved.
+//
+
+import XCTest
+@testable import RedCarpetAppleMVC
+
+class RedCarpetAppleMVCTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
