4b660e00ade97ca16491a881e73f824c63e1f868
Goksel Koksal <gokselkoksal@gmail.com>
2017-10-07 19:08:34 +0100

    Rename target to RedCarpetMVC

--- RedCarpetMVC/ViewController.swift
+++ RedCarpetMVC/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import Commons
 
 class ViewController: UIViewController {
+    
+    let service: MovieServiceProtocol = MovieService()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        service.fetchMovies { (result) in
+            print(result)
+        }
     }
 
     override func didReceiveMemoryWarning() {
